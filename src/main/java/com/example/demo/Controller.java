e9aed5b71406877dc539609f6c492333549074f9
kiki202181 <usagi211neko@yahoo.co.jp>
2021-08-29 14:41:17 +0900

    HelloWorld

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class Controller {
+
+	@RequestMapping("/")
+	public String index() {
+		return "hello";
+	}
+}
